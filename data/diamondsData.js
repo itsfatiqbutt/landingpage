777d9d1d6f80fe1c22cb73023b8a28684c72aff3
fatiqbutt <itsfatiqbutt@gmail.com>
2025-09-04 20:44:27 +0500

    Fixed diamond order

--- data/diamondsData.js
+++ data/diamondsData.js
@@ -1,49 +1,47 @@
 export const diamondsData =
 [
-
-{
-  img:"/images/round.png",
-  link: "Round"
-},
-{
-  img:"/images/oval.png",
-  link: "Oval"
-},
-{
-  img:"/images/emerald.png",
-  link: "Emerald"
-},
-{
-  img:"/images/asscher.png",
-  link: "Asscher"
-},
-{
-  img:"/images/radiant.png",
-  link: "Radiant"
-},
-{
-  img:"/images/elogant cushion.png",
-  link: "Elongated Cushion"
-},
-{
-  img:"/images/pear.png",
-  link: "Pear"
-},
-{
-  img:"/images/princess.png",
-  link: "Princess"
-},
-{
-  img:"/images/marquise.png",
-  link: "Marquise"
-},
-{
-  img:"/images/heart 2.png",
-  link: "Heart"
-},
-{
-  img:"/images/cushion.png",
-  link: "Cushion"
-},
-
+  {
+    img:"/images/princess.png",
+    link: "Princess"
+  },
+  {
+    img:"/images/round.png",
+    link: "Round"
+  },
+  {
+    img:"/images/emerald.png",
+    link: "Emerald"
+  },
+  {
+    img:"/images/oval.png",
+    link: "Oval"
+  },
+  {
+    img:"/images/radiant.png",
+    link: "Radiant"
+  },
+  {
+    img:"/images/pear.png",
+    link: "Pear"
+  },
+  {
+    img:"/images/cushion.png",
+    link: "Cushion"
+  },
+  {
+    img:"/images/marquise.png",
+    link: "Marquise"
+  },
+  {
+    img:"/images/asscher.png",
+    link: "Asscher"
+  },
+  {
+    img:"/images/elogant cushion.png",
+    link: "Elongated Cushion"
+  },
+  {
+    img:"/images/heart 2.png",
+    link: "Heart"
+  },
 ]
